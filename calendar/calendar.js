53276cbcc2c7c6994f6954daa21df3fee0c0fbb0
Bobby Steckline <rsteckline@gmail.com>
2024-08-24 19:19:24 -0400

    Added completed calendar tests

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,36 @@
+function createEvent(title, month, day) {
+    var event = {
+        title: title,
+        month: month,
+        day: day
+    }
+    if (day === 0 || day > 31) {
+        return `Error: ${day} is not a valid day`
+    }
+    return event
+}
+
+function createCalendar(owner, events) {
+    var calendar = {
+        owner: owner,
+        events: events
+    }
+    return calendar
+}
+
+function reportMonthlyEvents(calendarObj, month) {
+    var sameEvents = []
+    for (var i = 0; i < calendarObj.events.length; i++) {
+        if (calendarObj.events[i].month === month) {
+            sameEvents.push(calendarObj.events[i])
+        }
+    }
+return sameEvents
+}
 
 
-module.exports = { createBirthday, celebrateBirthday, countBirthdays };
+module.exports = { 
+    createEvent,
+    createCalendar, 
+    reportMonthlyEvents 
+};
