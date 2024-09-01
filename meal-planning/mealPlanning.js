465f63a4ae9d82dad80c8db045025d98a8aec024
Bobby Steckline <rsteckline@gmail.com>
2024-09-01 16:09:09 -0400

    Add completed mealPlanning tests

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,32 @@
+function createMeal(type, calorieGoal) {
+    var meal = {
+        type: type,
+        calorieGoal,
+        dishes: []
+    }
+    return meal
+}
 
-module.exports = { }
+function addDish(mealObj, dishObj) {
+    if (dishObj.calories <= mealObj.calorieGoal) {
+    mealObj.dishes.push(dishObj);
+    mealObj.calorieGoal = mealObj.calorieGoal - dishObj.calories
+    }
+    return mealObj
+}
+
+function calculateCalories(mealObj) {
+    var totalCal = 0;
+    for (var i = 0; i < mealObj.dishes.length; i++) {
+        totalCal += mealObj.dishes[i].calories
+    }
+    return `${mealObj.type} has a total of ${totalCal} calories`
+}
+
+
+
+module.exports = { 
+    createMeal, 
+    addDish, 
+    calculateCalories 
+}
